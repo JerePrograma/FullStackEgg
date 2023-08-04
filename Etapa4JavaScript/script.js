e898eae2a97bdc83790eeb69d1aa417c7942c25e
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-08-04 19:55:10 -0300

    "hola mundo"

---
+++ Etapa4JavaScript/script.js
@@ -0,0 +1,76 @@
+/*var edad = prompt("Qye edad tenés", 29);
+
+alert(`Hola, tu edad es ${edad}`);
+
+var mensaje = `Hola...
+
+${edad}`;
+
+console.log(mensaje);
+
+var persona = {
+  apellido: "Rivelli",
+  nombre: "Jeremias",
+  documento: 38430740,
+  nacimiento: new Date("15-02-1995"),
+  mascotas: [
+    {
+      apodo: "Chiquito",
+      numeroIdentificatorio: 123412,
+      nacimiento: new Date("01-02-2006"),
+    },
+    {
+      apodo: "Dulcito",
+      numeroIdentificatorio: 321451,
+      nacimiento: new Date("01-05-2006"),
+    },
+  ],
+};
+
+console.log(persona);
+
+/*var mascota = {
+    apodo : "Chiquito",
+    numeroIdentificatorio : 123412,
+    nacimiento : new Date("01-02-2006"),
+};
+
+class Mascota {
+  apodo;
+  numeroIdentificatorio;
+}
+
+var m = new Mascota();
+m.apodo = "Filomena";
+m.numeroIdentificatorio = 1293;
+
+console.log(m);
+console.log(persona.mascotas[1].apodo);
+console.log(JSON.stringify(numeros[0]));
+
+var numeros = [1, 2, 3, 4, 5, 6, 7];
+
+var personas = [
+  { nombre: "Agustin", apellido: "Fiorde" },
+  { nombre: "Jeremias", apellido: "Rivelli" },
+  { nombre: "Lujan", apellido: "Bravo" },
+];
+
+console.log(personas[personas.length - 1].apellido);
+
+
+let suma = (num1,num2) => num1+num2;
+console.log(suma(2,4));
+
+let saludar = () => console.log("Hola!");
+saludar();
+*/
+var nombre = prompt("Hola! Ingresá tu nombre");
+
+function saludar(nombre) {
+  alert(`Hola ${nombre}`);
+}
+
+saludar(nombre);
+
+addEventListener()
